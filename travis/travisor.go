2ab71975b038db3f79d70e804cc418b08545fefa
Adam Schmideg <adam@schmideg.net>
2020-02-18 12:08:35 +0100

    Read generic json from url and unmarshall it

---
+++ travis/travisor.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"flag"
+	"fmt"
+	"net/http"
+)
+
+const (
+	baseUrl = "https://api.travis-ci.org"
+)
+
+// type buildInfo []interface{}
+type buildInfo map[string]interface{}
+
+func readJSONFromUrl(path string) ([]byte, error) {
+	url := baseUrl + path
+	resp, err := http.Get(url)
+	if resp != nil {
+		defer resp.Body.Close()
+	}
+	if err != nil {
+		return nil, err
+	}
+
+	buf := new(bytes.Buffer)
+	if _, err := buf.ReadFrom(resp.Body); err != nil {
+		return nil, err
+	}
+	return buf.Bytes(), nil
+}
+
+func builds(repo string, maxBuilds int) (buildInfo,error) {
+	var jsonBytes []byte
+	var err error
+	if jsonBytes, err = readJSONFromUrl("/builds/650345062"); err != nil {
+		return nil, err
+	}
+	var b buildInfo
+	if err := json.Unmarshal(jsonBytes, &b); err != nil {
+		return nil, err
+	}
+	return b, nil
+}
+
+func main() {
+	repo := flag.String("repo", "ethereum/go-ethereum", "<username>/<repo> on github")
+	maxBuilds := flag.Int("max-builds", 5, "Max number of builds")
+	flag.Parse()
+	b, err := builds(*repo, *maxBuilds)
+	if err != nil {
+		fmt.Print(err)
+	}
+	fmt.Print("builds: ", b)
+}
