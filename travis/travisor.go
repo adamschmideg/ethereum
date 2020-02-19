2856027a12a3069dca13a03e3f2070927a2952d0
Adam Schmideg <adam@schmideg.net>
2020-02-19 11:52:38 +0100

    Paginate over builds

--- travis/travisor.go
+++ travis/travisor.go
@@ -9,6 +9,7 @@ import (
 )
 
 func failedBuilds(repo string, n int, buildStates []string, jobStates []string) {
+	//c := make(chan travis.Log)
 	jobsOpts := travis.JobsOption{State:jobStates}
 	client := travis.NewClient(travis.ApiOrgUrl, "")
 	offset := 0
@@ -17,8 +18,8 @@ func failedBuilds(repo string, n int, buildStates []string, jobStates []string) 
 		buildOpts := travis.BuildsByRepoOption{Offset:offset, Limit:limit, State: buildStates}
 		builds, _, _ := client.Builds.ListByRepoSlug(context.Background(), repo, &buildOpts)
 		for _, b := range builds {
-			fmt.Println("build", *b.StartedAt, *b.State)
 			jobs, _, _ := client.Jobs.List(context.Background(), &jobsOpts)
+			fmt.Println("build", *b.StartedAt, *b.State, len(jobs))
 			for _, j := range jobs {
 				log, _, _ := client.Logs.FindByJobId(context.Background(), *j.Id)
 				fmt.Println(*log.Href)
@@ -30,7 +31,7 @@ func failedBuilds(repo string, n int, buildStates []string, jobStates []string) 
 
 func main() {
 	repo := flag.String("repo", "ethereum/go-ethereum", "Github <username>/<repo>")
-	buildCount := flag.Int("build.count", 5, "Number of builds to check")
+	buildCount := flag.Int("build.count", 2, "Number of builds to check")
 	bs := flag.String("build.states", "failed,errored", "Comma-separated list of build states")
 	buildStates := strings.Split(*bs, ",")
 	js := flag.String("job.states", "failed,errored", "Comma-separated list of job states")
