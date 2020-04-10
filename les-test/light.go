e389e5ab67984dda19abbaa83ca6584f7cbb4a38
Adam Schmideg <adam@schmideg.net>
2020-04-10 18:40:15 +0200

    Use go-ethereum/rpc

--- les-test/light.go
+++ les-test/light.go
@@ -5,25 +5,44 @@ import (
 	"log"
 	"os"
 	"os/exec"
+
+	"github.com/ethereum/go-ethereum/p2p"
+	"github.com/ethereum/go-ethereum/rpc"
 )
+
+type client struct {
+	c      *rpc.Client
+}
 
 func startServer(rpcport int, networkid int) error {
 	cmd := exec.Command("docker-compose", "-f", "server.yml", "run", "--service-ports", "--no-deps", "lightserver")
 	cmd.Env = append(os.Environ(), fmt.Sprintf("RPCPORT=%v", rpcport), fmt.Sprintf("NETWORKID=%v", networkid))
-	stdoutStderr, err := cmd.CombinedOutput()
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	err := cmd.Start()
 	if err != nil {
 		return err
 	}
-	fmt.Printf("%s\n", stdoutStderr)
+	fmt.Printf("running")
 	return nil
 }
 
 func main() {
+	var err error
 	port := 8548
 	networkid := 5 // goerli
-	err := startServer(port, networkid)
+	err = startServer(port, networkid)
 	if err != nil {
 		log.Fatal(err)
 	}
-	// rpc call it
+
+	cli := client{}
+	url := fmt.Sprintf("http://127.0.0.1:%v", port)
+	cli.c, err = rpc.Dial(url)
+	if err != nil {
+		log.Fatal(err)
+	}
+	nodeInfo := &p2p.NodeInfo{}
+	cli.c.Call(&nodeInfo, "admin_nodeInfo")
+	fmt.Println("nodeInfo", nodeInfo)
 }
