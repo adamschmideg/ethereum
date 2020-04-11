db2dac8bdc4d809af8523f71417c582cf05f03db
Adam Schmideg <adam@schmideg.net>
2020-04-11 12:26:34 +0200

    addPeer with notification which does not work

--- les-test/light.go
+++ les-test/light.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"os"
 	"os/exec"
+	"time"
 
 	"github.com/ethereum/go-ethereum/p2p"
 	"github.com/ethereum/go-ethereum/rpc"
@@ -27,22 +29,78 @@ func startServer(rpcport int, networkid int) error {
 	return nil
 }
 
-func main() {
-	var err error
-	port := 8548
-	networkid := 5 // goerli
-	err = startServer(port, networkid)
-	if err != nil {
-		log.Fatal(err)
-	}
+var portToClient = make(map[int]*client)
 
-	cli := client{}
+func connect(port int) (*client, error) {
+	cli, _ := portToClient[port]
+	if cli != nil {
+		// already connected
+		return cli, nil
+	}
+	cli = &client{}
 	url := fmt.Sprintf("http://127.0.0.1:%v", port)
+	var err error
 	cli.c, err = rpc.Dial(url)
 	if err != nil {
+		return nil, err
+	}
+	portToClient[port] = cli
+	return cli, nil
+}
+
+func (cli *client) nodeInfo() (*p2p.NodeInfo, error) {
+	nodeInfo := &p2p.NodeInfo{}
+	err := cli.c.Call(&nodeInfo, "admin_nodeInfo")
+	if err != nil {
+		return nil, err
+	}
+	return nodeInfo, nil
+}
+
+func addPeer(serverPort int, clientPort int) error {
+	server, err := connect(serverPort)
+	if err != nil {
+		log.Fatal("Cannot connect server", err)
+	}
+	client, err := connect(clientPort)
+	if err != nil {
+		log.Fatal("Cannot connect client", err)
+	}
+
+	nodeInfo, err := server.nodeInfo()
+	if err != nil {
+		return err
+	}
+	enode := nodeInfo.Enode
+	peerCh := make(chan *p2p.PeerEvent)
+	sub, err := server.c.Subscribe(context.Background(), "admin", peerCh, "peerEvents")
+	if err != nil {
+		return err
+	}
+	defer sub.Unsubscribe()
+	err = client.c.Call(nil, "admin_addPeer", enode)
+	if err != nil {
+		return err
+	}
+	dur := 14 * time.Second
+	timeout := time.After(dur)
+	select {
+	case ev := <-peerCh:
+		fmt.Printf("At port %v received event: type=%v, peer=%v", serverPort, ev.Type, ev.Peer)
+	case err := <-sub.Err():
+		return err
+	case <-timeout:
+		return fmt.Errorf("Timeout after %v", dur)
+	}
+	return nil
+}
+
+func main() {
+	serverPort := 8548
+	clientPort := 9548
+
+	err := addPeer(serverPort, clientPort)
+	if err != nil {
 		log.Fatal(err)
 	}
-	nodeInfo := &p2p.NodeInfo{}
-	cli.c.Call(&nodeInfo, "admin_nodeInfo")
-	fmt.Println("nodeInfo", nodeInfo)
 }
