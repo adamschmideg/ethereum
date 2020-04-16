edd9a40be6462452280aa96c84a44fe6905f19df
Adam Schmideg <adam@schmideg.net>
2020-04-16 19:30:31 +0200

    curl uses container_name

--- les-test/spam.sh
+++ les-test/spam.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 ethspam | while read q                                                       
 do
-	echo curl http://localhost:9441  -H "Content-Type: application/json" -X POST --data $q
-	curl http://localhost:9441  -H "Content-Type: application/json" -X POST --data $q
-	curl http://localhost:9442  -H "Content-Type: application/json" -X POST --data $q
+	echo $q
+	curl http://client_1:9444  -H "Content-Type: application/json" -X POST --data $q
+	curl http://client_2:9444  -H "Content-Type: application/json" -X POST --data $q
 done
