425f6a71382289085725ece5fda0ab8ff6cc30e6
Adam Schmideg <adam@schmideg.net>
2020-04-20 17:45:13 +0200

    addPeer with a single docker-compose.yml -- two yaml files had a problem with the network

--- les-test/spam.sh
+++ les-test/spam.sh
@@ -1,7 +1,16 @@
 #!/bin/sh
-ethspam | while read q                                                       
-do
-	echo $q
-	curl http://client_1:9444  -H "Content-Type: application/json" -X POST --data $q
-	curl http://client_2:9444  -H "Content-Type: application/json" -X POST --data $q
-done
+alias send='curl -s -H "Content-Type: application/json" -X POST --data '
+enode=`send '{"jsonrpc":"2.0","id":555,"method":"admin_nodeInfo","params":[]}' http://lightserver:8444 | jq -r .result.enode`
+json="{\"jsonrpc\":\"2.0\",\"id\":555,\"method\":\"admin_addPeer\",\"params\":[\"${enode}\"]}"
+echo "sending: $json"
+send $json http://client_1:9444
+json="{\"jsonrpc\":\"2.0\",\"id\":555,\"method\":\"admin_peers\",\"params\":[]}"
+send $json http://client_1:9444
+sleep 2000
+
+#ethspam | while read q
+#do
+#	echo $q
+#	curl http://client_1:9444  -H "Content-Type: application/json" -X POST --data $q
+#	curl http://client_2:9444  -H "Content-Type: application/json" -X POST --data $q
+#done
