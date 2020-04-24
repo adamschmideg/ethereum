8add92df8f3ea1a1ff999c77a97c664438b46115
Adam Schmideg <adam@schmideg.net>
2020-04-24 17:40:36 +0200

    simple query works

--- stats/gql.go
+++ stats/gql.go
@@ -2,19 +2,19 @@ package main
 
 import (
 	"context"
+	"flag"
 	"fmt"
 	"github.com/machinebox/graphql"
 )
 
 func main() {
+	query := flag.String("q", "query { viewer { login } }", "GraphQL query string")
+	token := flag.String("token", "<token>", "Github token")
+	flag.Parse()
+
 	client := graphql.NewClient("https://api.github.com/graphql")
-	q := `
-		query {
-			viewer {
-				login
-			}
-		}`
-	request := graphql.NewRequest(q)
+	request := graphql.NewRequest(*query)
+	request.Header.Set("Authorization", fmt.Sprintf("bearer %v", *token))
 	var response interface{}
 	err := client.Run(context.Background(), request, &response)
 	if err != nil {
